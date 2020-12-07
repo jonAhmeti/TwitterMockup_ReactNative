56c94eba664d86492581881ab1f023ad8479c270
Jon Ahmeti <jon-ahmeti@hotmail.com>
2020-12-07 15:24:29 +0100

    Added ReactNavigation 5 lib

--- components/Login/Header.js
+++ components/Login/Header.js
@@ -1,46 +1,42 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
-import AntDesign from 'react-native-vector-icons/AntDesign'
+import {View, Text, StyleSheet} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
-
-const Header = (props) =>{
-
-    return(
-      <View style={styles.container}>
-          <AntDesign name="twitter" style={styles.logo}/>
-          <View style={styles.logoTextWrapper}>
-            <Text style={styles.logoText}>{props.text}</Text>
-          </View>
+const Header = (props) => {
+  return (
+    <View style={styles.container}>
+      <AntDesign name="twitter" style={styles.logo} />
+      <View style={styles.logoTextWrapper}>
+        <Text style={styles.logoText}>{props.text}</Text>
       </View>
-    );
+    </View>
+  );
 };
 
-
 Header.defaultProps = {
-    text : 'See what’s happening in the world right now'
-}
+  text: 'See what’s happening in the world right now',
+};
 
 const styles = StyleSheet.create({
-    container:{
-        paddingLeft:40,
-        paddingRight:40,
-        alignSelf:'stretch',
-   },
-    logo:{
-        fontSize: 70,
-        color:'#ffffff',
-        textShadowColor:'black',
-    },
-    logoTextWrapper:{
-        marginLeft:40,
-        marginTop:5,
-    },
-    logoText:{
-        fontSize:22,
-        fontWeight:'bold',
-        color:'#ffffff',
-    },
-
+  container: {
+    paddingLeft: 40,
+    paddingRight: 40,
+    alignSelf: 'stretch',
+  },
+  logo: {
+    fontSize: 70,
+    color: '#ffffff',
+    textShadowColor: 'black',
+  },
+  logoTextWrapper: {
+    marginLeft: 40,
+    marginTop: 5,
+  },
+  logoText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#ffffff',
+  },
 });
 
 export default Header;
